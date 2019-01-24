14e148b43fe7e84cbdbdb7c875e9db21e38a0ed0
Andy Wirtz <andyw@atix.de>
2019-01-24 13:45:57 +0100

    fix name

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,4 +1,6 @@
-class role::web {
-  include ::profile::base
-  include ::profile::apache
+class v_role_web::web {
+  include ::v_profile_base::base
+  include ::v_profile_apache::apache
+  include ::v_profile_docker::docker
+  include ::v_profile_selinux::selinux
 }
